b5974c035de859d551278cfe81eb172c014ac327
ClaudiaInBytes <claudiainbytes@gmail.com>
2017-08-06 01:18:39 -0500

    restoring changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 
 gulp.task('minify-css', () => {
-  return gulp.src('./src/**/*.css')
+  return gulp.src('./src/css/**/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('./css'));
 });
 // Permite ejecutar tareas por defecto
 
