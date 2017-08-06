c79ef44e52a29965a2b26deda87c8f2bdd7765b0
ClaudiaInBytes <claudiainbytes@gmail.com>
2017-08-06 00:43:25 -0500

    Restore changes

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// Incluir plugins gulp y gulp-minify-css
+
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', () => {
+  return gulp.src('./src/**/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('css'));
+});
+// Permite ejecutar tareas por defecto
+
+gulp.task('default', ['minify-css']);
